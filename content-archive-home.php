8ed4266b5b90361cc22a6376e2c92cbceeaf1c6a
John Keith <johnkeith@outlook.com>
2015-07-25 12:18:49 -0400

    show featured image on first post of main loop

--- content-archive-home.php
+++ content-archive-home.php
@@ -1,9 +1,11 @@
 <div <?php post_class(); ?>>
 	<?php
-	// output Featured Image
-	echo '<a class="featured-image-link" href="' . get_permalink() . '">';
-		ct_tracks_featured_image();
-	echo '</a>';
+	// output Featured Image if first post
+	if($wp_query -> current_post == 0 && !is_paged()){
+			echo '<a class="featured-image-link" href="' . get_permalink() . '">';
+				ct_tracks_featured_image();
+			echo '</a>';
+		}
 	?>
 	<div class="excerpt-container">
 		<div class="excerpt-meta">
--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php 
 
-function number_of_posts_on_archive($query){
+function jk_number_of_posts_on_archive($query){
   if ($query->is_archive){
     $query->set('posts_per_page', 30);
 	}
   return $query;
 }
  
-add_filter('pre_get_posts', 'number_of_posts_on_archive');
+add_filter('pre_get_posts', 'jk_number_of_posts_on_archive');
+
