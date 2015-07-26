1ccce47e19a210d2c1e391f33dce1599dfaec119
John Keith <johnkeith@outlook.com>
2015-07-25 21:38:38 -0400

    showing only excerpt

--- content-archive-home.php
+++ content-archive-home.php
@@ -1,11 +1,11 @@
 <div <?php post_class(); ?>>
 	<?php
 	// output Featured Image if first post
-	if($wp_query -> current_post == 0 && !is_paged()){
+	// if($wp_query -> current_post == 0 && !is_paged()){
 			echo '<a class="featured-image-link" href="' . get_permalink() . '">';
 				ct_tracks_featured_image();
 			echo '</a>';
-		}
+		// }
 	?>
 	<div class="excerpt-container">
 		<div class="excerpt-meta">
@@ -18,7 +18,13 @@
 		</div>
 		<div class='excerpt-content'>
 			<article>
-				<?php ct_tracks_excerpt(); ?>
+				<?php 
+					if($wp_query -> current_post == 0 && !is_paged()){
+						ct_tracks_excerpt();
+					} else {
+						jk_tracks_excerpt();
+					} 
+				?>
 			</article>
 		</div>
 	</div>
--- functions.php
+++ functions.php
@@ -9,3 +9,23 @@ function jk_number_of_posts_on_archive($query){
  
 add_filter('pre_get_posts', 'jk_number_of_posts_on_archive');
 
+// custom excerpt handling
+if( ! function_exists( 'jk_tracks_excerpt' ) ) {
+    function jk_tracks_excerpt() {
+
+        // make post variable available
+        global $post;
+
+        // check for the more tag
+        $ismore = strpos( $post->post_content, '<!--more-->' );
+
+        // if ( $ismore ) {
+        //     the_content( __( 'Read More', 'tracks' ) . "<span class='screen-reader-text'>" . get_the_title() . "</span>" );
+        // } // otherwise the excerpt is automatic, so output it
+        // else {
+        //     the_excerpt();
+        // }
+        the_excerpt();
+    }
+}
+
